0cb41a5a5131cd55f3cf4f53aa4d4e18697cf869
Eduardo Laux <eduardodalaux@gmail.com>
2022-07-22 18:25:24 -0300

    Remoção footer

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu')
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "img/menu.png";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "img/close.png";
+/*===== MENU SHOW Y HIDDEN =====*/ 
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+
+/*SHOW*/ 
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+/*HIDDEN*/
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  navMenu.classList.remove('show')
 }
-}
+navLink.forEach(n => n.addEventListener('click', linkAction));
